d57ed155dce4ab30f100ce1be36b8d0d5c6a4fe3
Steven Zaluk <stevezaluk@gmail.com>
2022-08-03 12:56:52 -0400

    Loading config files now works. Also renamed the main function to run

--- api/api.py
+++ api/api.py
@@ -7,22 +7,34 @@ from asgard_sdk.models.config import Config
 
 class Rest(object):
     def __init__(self, config_path:str, debug=False):
+        self.config_path = config_path
+        self.debug = debug
+
+        self.endpoint_prefix = "/v1/api"
+
+        self._config = self.load_config()
         self._app = Flask(__name__)
         self._api = Api(app=self._app)
 
-        self.debug = debug
-        self.config_path = config_path
-
-        self.config = None
+    def get_config(self):
+        return self._config
 
     def load_config(self):
-        pass
+        config = Config(self.config_path)
+        config.validate_structure()
+        
+        values = config.values
+        for key in values:
+            value = values[key]
+            self._app.config[key] = value
+
+        return config
 
     def _register_teardowns(self):
         pass
 
     def _build_namespaces(self):
         pass
-
-    def start(self):
+    
+    def run(self):
         self._app.run(debug=self.debug)
