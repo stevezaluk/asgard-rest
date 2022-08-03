4fb6f325d173b523e10f105c6a42337bb9480b4f
Steven Zaluk <stevezaluk@gmail.com>
2022-08-03 12:58:29 -0400

    Wrote boilerplate for pushing resources to the request context

---
+++ api/context.py
@@ -0,0 +1,10 @@
+from flask import g, current_app
+
+from asgard_sdk.server.server import AsgardServer
+
+def get_resources(func):
+    def inner(*args, **kwargs):
+        pass
+
+def teardown_resources(exception):
+    pass
