e6f4f7b4b658732f43ff9647fea1db809b51a9bd
Steven Zaluk <stevezaluk@gmail.com>
2022-08-03 12:01:06 -0400

    Wrote boilerplate for Rest object

--- api/api.py
+++ api/api.py
@@ -3,9 +3,26 @@ import sys
 from flask import Flask
 from flask_restx import Api
 
+from asgard_sdk.models.config import Config
+
 class Rest(object):
-    def __init__(self, debug=False):
+    def __init__(self, config_path:str, debug=False):
         self._app = Flask(__name__)
         self._api = Api(app=self._app)
 
         self.debug = debug
+        self.config_path = config_path
+
+        self.config = None
+
+    def load_config(self):
+        pass
+
+    def _register_teardowns(self):
+        pass
+
+    def _build_namespaces(self):
+        pass
+
+    def start(self):
+        self._app.run(debug=self.debug)
